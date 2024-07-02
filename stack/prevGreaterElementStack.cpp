68b14c5d30c179b17f7ec1495ce626318046c303
Saurabh Kumar <meetsaurabh78@gmail.com>
2024-07-02 18:39:22 +0530

    prev greater element

---
+++ stack/prevGreaterElementStack.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+// if arr[i]<=st.top() then pop that element
+//else element is st.top is greater element than arr[i] then push it in ans[i];
+//and arr[i] element ko push karo stack mein because probability of being next greater element of next number of arr[i]
+//if stack is empty then mark ans[i]=-1;
+
+int main() {
+    int arr[] = {3, 1, 2, 7, 4, 6, 2, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    stack<int> st;
+    int ans[size];
+    //make last element of array -1 bcoz no next greater element than last element
+    ans[0]=-1;
+    st.push(arr[0]);
+//initially push last element of array into stack
+
+    for (int i = 1; i <size; i++) {
+       //pop all the element smaller than arr[i]
+        while (!st.empty() && st.top() <= arr[i]) {
+            st.pop(); 
+        }
+        //marks the ans array
+        if(!st.size()) ans[i]=-1;
+        else ans[i]=st.top();
+        //push the element arr[i]
+        st.push(arr[i]);
+    }
+
+
+    for (int i = 0; i < size; i++) {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
