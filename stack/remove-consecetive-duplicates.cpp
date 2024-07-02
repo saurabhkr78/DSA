3d5cf4ebca98b72b8bd21cffc699dea46037d63d
Saurabh Kumar <meetsaurabh78@gmail.com>
2024-07-02 14:55:06 +0530

    2 consecutive duplicates

---
+++ stack/remove-consecetive-duplicates.cpp
@@ -0,0 +1,30 @@
+
+
+#include<iostream>
+#include<stack>
+using namespace std;
+string removeDuplicates(string s){
+    stack<char>st;
+    st.push(s[0]);
+    for(int i=1;i<s.length();i++)
+        if(s[i]!=st.top()) st.push(s[i]);
+
+        //re-initalise s
+       s="";
+       while(st.size()>0){
+        s+=st.top();
+        st.pop();
+       }
+       return s;
+}
+
+
+
+
+int main(){
+    string s="abba dabba chabba";
+    
+    cout<<removeDuplicates(s)<<endl;
+    
+
+}
