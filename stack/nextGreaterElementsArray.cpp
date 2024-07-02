e71e7796130ab21b21b6aa4d25be29e7d7f84bad
Saurabh Kumar <meetsaurabh78@gmail.com>
2024-07-02 18:13:35 +0530

    actual concept

--- stack/nextGreaterElementsArray.cpp
+++ stack/nextGreaterElementsArray.cpp
@@ -2,24 +2,35 @@
 #include <stack>
 #include <vector>
 using namespace std;
+// if arr[i]<=st.top() then pop that element
+//else element is st.top is greater element than arr[i] then push it in ans[i];
+//and arr[i] element ko push karo stack mein because probability of being next greater element of next number of arr[i]
+//if stack is empty then mark ans[i]=-1;
 
 int main() {
-    int arr[] = {3, 10, 4, 2, 1, 2, 6, 1, 7, 2, 9};
+    int arr[] = {3, 1, 2, 7, 4, 6, 2, 3};
     int size = sizeof(arr) / sizeof(arr[0]);
-    vector<int> ans(size, -1); // Initialize ans with -1 indicating no next greater element found
+    
+    stack<int> st;
+    int ans[size];
+    //make last element of array -1 bcoz no next greater element than last element
+    ans[size-1]=-1;
+    st.push(arr[size-1]);
+//initially push last element of array into stack
 
-    stack<int> st; // Stack to keep track of indices
-
-    for (int i = 0; i < size; i++) {
-        // While stack is not empty and the current element is greater than the element at the index stored in st.top()
-        while (!st.empty() && arr[st.top()] < arr[i]) {
-            ans[st.top()] = arr[i]; // Update ans for the index where we found the next greater element
-            st.pop(); // Pop the element from stack
+    for (int i = size-2; i >=0; i--) {
+       //pop all the element smaller than arr[i]
+        while (!st.empty() && st.top() <= arr[i]) {
+            st.pop(); 
         }
-        st.push(i); // Push the current index onto the stack
+        //marks the ans array
+        if(!st.size()) ans[i]=-1;
+        else ans[i]=st.top();
+        //push the element arr[i]
+        st.push(arr[i]);
     }
 
-    // Print the ans array
+
     for (int i = 0; i < size; i++) {
         cout << ans[i] << " ";
     }
