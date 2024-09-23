1038afffb2c7d8c3e59d53a853267685194dcc16
Saurabh Kumar <meetsaurabh78@gmail.com>
2024-09-23 18:17:10 +0530

    first and last occurence +count of an element

--- binary-search/1st_and_last_occurence.cpp
+++ binary-search/1st_and_last_occurence.cpp
@@ -3,39 +3,58 @@ using namespace std;
 
 int findFirstOccurrence(int arr[], int n, int x)
 {
-    int i=0,j=n-1;
-    int res=-1;
-    while(i<=j){
-        int mid=(i+(j-i)/2);
-       if (arr[mid] == x) {  // If element is found
-            res = mid;       // Store the index
-            j = mid - 1;     // Continue searching in the left half
+    int i = 0, j = n - 1;
+    int res = -1;
+    while (i <= j)
+    {
+        int mid = (i + (j - i) / 2);
+        if (arr[mid] == x)
+        {                // If element is found
+            res = mid;   // Store the index
+            j = mid - 1; // Continue searching in the left half
         }
-        else if(arr[mid]>x){
-            j=mid-1;//search in left half
+        else if (arr[mid] > x)
+        {
+            j = mid - 1; // search in left half
         }
-        else if(arr[mid]<x){
-            i=mid+1;//search in right
+        else if (arr[mid] < x)
+        {
+            i = mid + 1; // search in right
         }
     }
-  return res;
+    return res;
+}
+int findLastOccurence(int arr[], int n, int x)
+{
+    int i = 0, j = n - 1;
+    int res = -1;
+    while (i <= j)
+    {
+        int mid = (i + (j - i) / 2);
+        if (arr[mid] == x)
+        {                // If element is found
+            res = mid;   // Store the index
+            i = mid + 1; // Continue searching in the right half
+        }
+        else if (arr[mid] > x)
+        {
+            j = mid - 1; // search in left half
+        }
+        else if (arr[mid] < x)
+        {
+            i = mid + 1; // search in right
+        }
+    }
+    return res;
 }
 
 int main()
 {
     int arr[] = {1, 2, 2, 2, 2, 3, 4, 7, 8, 8};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 8;
-
-    int result = findFirstOccurrence(arr, n, x);
-    if (result != -1)
-    {
-        cout << "First occurrence of " << x << " is at index: " << result << endl;
-    }
-    else
-    {
-        cout << x << " not found in the array" << endl;
-    }
-
+    int x = 2;
+    int firstAppearance = findFirstOccurrence(arr, n, x);
+    int lastAppearance = findLastOccurence(arr, n, x);
+    cout << "first Appearance:" << firstAppearance << " " << "last Apperance:" << lastAppearance << endl;
     return -1;
 }
---
+++ binary-search/count_elem.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findFirstOccurrence(int arr[], int n, int x)
+{
+    int i=0,j=n-1;
+    int res=-1;
+    while(i<=j){
+        int mid=(i+(j-i)/2);
+       if (arr[mid] == x) {  // If element is found
+            res = mid;       // Store the index
+            j = mid - 1;     // Continue searching in the left half
+        }
+        else if(arr[mid]>x){
+            j=mid-1;//search in left half
+        }
+        else if(arr[mid]<x){
+            i=mid+1;//search in right
+        }
+    }
+  return res;
+}
+int findLastOccurence(int arr[], int n, int x){
+     int i=0,j=n-1;
+    int res=-1;
+    while(i<=j){
+        int mid=(i+(j-i)/2);
+       if (arr[mid] == x) {  // If element is found
+            res = mid;       // Store the index
+            i = mid + 1;     // Continue searching in the right half
+        }
+        else if(arr[mid]>x){
+            j=mid-1;//search in left half
+        }
+        else if(arr[mid]<x){
+            i=mid+1;//search in right
+        }
+    }
+  return res;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 2, 2, 2, 3, 4, 7, 8, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 2;
+    int firstAppearance=findFirstOccurrence(arr,n,x);
+    int lastAppearance=findLastOccurence(arr,n,x);
+
+    int count =(lastAppearance-firstAppearance+1);
+    cout<<count<<"";
+    return -1;
+}
